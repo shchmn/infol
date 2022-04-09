c474ae0b61806f9a94cc0c440c69f7fa2f29f1df
feride <shchmnv@gmail.com>
2022-04-10 02:43:41 +0500

    Less_4

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-# This is a sample Python script.
+from lxml import html
+import requests
+from pprint import pprint
+from pymongo import MongoClient
+from pymongo.errors import DuplicateKeyError
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+url = 'https://lenta.ru/'
+header = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36'}
+response = requests.get(url, headers=header)
+dom = html.fromstring(response.text)
 
+client = MongoClient('127.0.0.1', 27017)
+db = client['news']
+newsdb = db.newsdb
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+news_list = dom.xpath("//a[contains(@class,'_topnews')]")
+news = []
+for newsi in news_list:
+    news_info = {}
+    title = newsi.xpath(".//h3[@class='card-big__title']/text() | "".//span[contains(@class,'card-mini__title')]/text()")
+    link = newsi.xpath("./@href")
+    time = newsi.xpath(".//time/text()")
+    sourse = 'https://lenta.ru/'
 
+    news_info['title'] = title
+    news_info['link'] = link
+    news_info['time'] = time
+    news_info['sourse'] = sourse
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    try:
+        newsdb.insert_one(news_info)
+    except DuplicateKeyError:
+        print(f"Document  {news_info['title']} already exist")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    result = list(newsdb.find({}))
+    pprint(result)
